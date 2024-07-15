7a599e09b00d4bb1259e6be167f89614796a6217
Se-myung-Jin <tpaud94@naver.com>
2024-07-15 17:53:41 +0900

    SpinLock 추가

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -2,9 +2,40 @@
 {
     class Program
     {
+        static int num = 0;
+        static SpinLock spinLock = new SpinLock();
+
         static void Main(string[] args)
         {
-            
+            Task task1 = new Task(Task1);
+            Task task2 = new Task(Task2);
+
+            task1.Start();
+            task2.Start();
+
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine($"num : {num}");
+        }
+
+        static void Task1()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                spinLock.AcquireV2();
+                num++;
+                spinLock.Release();
+            }
+        }
+
+        static void Task2()
+        {
+            for (int j = 0; j < 100000; j++)
+            {
+                spinLock.AcquireV2();
+                num--;
+                spinLock.Release();
+            }
         }
     }
 }
---
+++ CSharpStudy/SpinLock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    public class SpinLock
+    {
+        int locked = 0;
+
+        public void Acquire()
+        {
+            while (true)
+            {
+                int original = Interlocked.Exchange(ref locked, 1);
+                if (original == 0)
+                    break;
+            }
+        }
+
+        public void Release()
+        {
+            locked = 0;
+        }
+
+        public void AcquireV2()
+        {
+            while (true)
+            {
+                if (Interlocked.CompareExchange(ref locked, 1, 0) == 0)
+                    break;
+            }
+        }
+    }
+}
